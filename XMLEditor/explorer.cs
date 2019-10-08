e08ce26624ac7f8dc5610f70fedce04550baf8f8
samufromhungary <blzkproblem@gmail.com>
2019-10-08 11:30:46 +0200

    explorer done (minor bug: when exiting without selecting throws error)

---
+++ XMLEditor/explorer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XMLEditor
+{
+    public class Explorer
+    {
+        public Explorer()
+        {
+
+        }
+
+        public String SelectedFilePath()
+        {
+            String path = "";
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "xml files (*.xml)|*.xml|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 1;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    path = openFileDialog.FileName;
+                }
+            }
+            return path;
+        }
+    }
+}
